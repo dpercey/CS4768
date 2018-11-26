1ffe2366e866bdc4f2a333124a958da3aa4e433f
Dylan <dylanxcode@empire.cs.mun.ca>
2018-11-26 13:26:33 -0330

    fg

--- FinalProject/SecondViewController.m
+++ FinalProject/SecondViewController.m
@@ -17,13 +17,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    CLLocationManager* locationManager;
+    locationManager = [[CLLocationManager alloc]init];
+    locationManager.delegate = self;
+    [locationManager startUpdatingLocation];
+    
+    CLLocationCoordinate2D currentLocation;
+    
+    currentLocation.latitude=locationManager.location.coordinate.latitude;
+    currentLocation.longitude=locationManager.location.coordinate.longitude;
+    
+    
+    MKCoordinateRegion reg = MKCoordinateRegionMakeWithDistance(currentLocation, 400, 400);
+    self.mapView.region = reg;
+    self.mapView.delegate = self;
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
+    [self.mapView setRegion:[self.mapView regionThatFits:region] animated:YES];
 }
 
 
